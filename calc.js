5abb0a5e241ec2e9234d95f28250ebf57e27ca74
TheDermot <88741983+TheDermot@users.noreply.github.com>
2025-02-19 21:36:40 -0500

    Created operate func
    Takes two numbers and one string as the operator to decide math func

--- calc.js
+++ calc.js
@@ -10,11 +10,11 @@ const subtract = function (a, b) {
   return a - b;
 };
 
-const multiply = function (a,b) {
+const multiply = function (a, b) {
   return a * b;
 };
 
-const divide = function (a,b) {
+const divide = function (a, b) {
   return a / b;
 };
 
@@ -23,3 +23,14 @@ const power = function (a, b) {
 };
 
 //calc
+
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+
+const operate = (num1, operator, num2) => {
+  if (operator == "add") return add(num1, num2);
+  else if ((operator == "subtract")) return subtract(num1, num2);
+  else if ((operator == "multiply")) return multiply(num1, num2);
+  else if ((operator == "divide")) return divide(num1, num2);
+};
