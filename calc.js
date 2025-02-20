6e8c34f98b7aa723f31da58dfd14e2ff8e8d4692
TheDermot <88741983+TheDermot@users.noreply.github.com>
2025-02-19 21:27:31 -0500

    Added divid func and changed each func for 2 param

--- calc.js
+++ calc.js
@@ -10,24 +10,16 @@ const subtract = function (a, b) {
   return a - b;
 };
 
-const sum = function (arr) {
-  if (arr.length == 0) return 0;
-  return arr.reduce((a, b) => a + b);
+const multiply = function (a,b) {
+  return a * b;
 };
 
-const multiply = function (arr) {
-  return arr.reduce((a, b) => a * b);
+const divide = function (a,b) {
+  return a / b;
 };
 
 const power = function (a, b) {
   return a ** b;
 };
 
-// const factorial = function (a) {
-//   if (a == 0 || a == 1) return 1;
-//   let temp = 1;
-//   for (let i = 2; i <= a; i++) {
-//     temp = temp * i;
-//   }
-//   return temp;
-// };
+//calc
