3be670f0db016f5f5743ce6b8250f641f748a2e8
TheDermot <88741983+TheDermot@users.noreply.github.com>
2025-02-19 21:22:17 -0500

    Added some math function to test in console

--- calc.js
+++ calc.js
@@ -1,3 +1,33 @@
 // calc.js
 console.log("JavaScript is working!");
 
+//math functions
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const sum = function (arr) {
+  if (arr.length == 0) return 0;
+  return arr.reduce((a, b) => a + b);
+};
+
+const multiply = function (arr) {
+  return arr.reduce((a, b) => a * b);
+};
+
+const power = function (a, b) {
+  return a ** b;
+};
+
+// const factorial = function (a) {
+//   if (a == 0 || a == 1) return 1;
+//   let temp = 1;
+//   for (let i = 2; i <= a; i++) {
+//     temp = temp * i;
+//   }
+//   return temp;
+// };
