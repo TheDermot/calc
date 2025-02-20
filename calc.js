ee2fe0f26998d859e392e0f8fc8aada1d7c82499
TheDermot <88741983+TheDermot@users.noreply.github.com>
2025-02-20 15:12:51 -0500

    ADded classes and id to buttons

--- calc.js
+++ calc.js
@@ -1,6 +1,8 @@
 // calc.js
 console.log("JavaScript is working!");
 
+
+
 //math functions
 const add = function (a, b) {
   return a + b;
@@ -29,8 +31,12 @@ let num2 = 0;
 let operator = "";
 
 const operate = (num1, operator, num2) => {
-  if (operator == "add") return add(num1, num2);
-  else if ((operator == "subtract")) return subtract(num1, num2);
-  else if ((operator == "multiply")) return multiply(num1, num2);
-  else if ((operator == "divide")) return divide(num1, num2);
+  if (operator == "+") return add(num1, num2);
+  else if ((operator == "-")) return subtract(num1, num2);
+  else if ((operator == "*")) return multiply(num1, num2);
+  else if ((operator == "/")) return divide(num1, num2);
+  else return null
 };
+
+
+let displayValue = document.querySelector(".display p");
