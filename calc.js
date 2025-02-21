0a90b27b0cda2294d5a4faf768641b7f20e9c433
TheDermot <88741983+TheDermot@users.noreply.github.com>
2025-02-21 13:53:50 -0500

    Reordered js code for readability

--- calc.js
+++ calc.js
@@ -1,5 +1,6 @@
 // calc.js
 
+//DOM Elements
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const deleteBtn = document.getElementById("delete");
@@ -8,81 +9,30 @@ const equalBtn = document.getElementById("equal");
 const decimalBtn = document.getElementById("decimal");
 let displayScreen = document.querySelector(".display p");
 
+//Global variables
 let num1 = "";
 let num2 = "";
 let operator = "";
 let result;
-let decimalClickedAfterEqual = false;
+let decimalClickedAfterEqual = false; //decimal tracker so decimal  can be clicked after =
 const maxDigits = 23; //based off casio user guide
 
-//listener on numbers buttons
-numbers.forEach((num) => {
-  num.addEventListener("click", () => {
-    if (operator === "" && result !== undefined) {
-      //if evaluation occured and number is clicked next instead of op
-      if (decimalClickedAfterEqual);
-      else num1 = "";
-      num2 = "";
-      result = undefined; // Reset result
-    }
-    if (num1.length < maxDigits) {
-      num1 += num.textContent; // Always append to num1
-      displayScreen.textContent = num1;
-      decimalClickedAfterEqual = false; // Reset the flag after a number is clicked
-    }
-  });
-});
+//window event listener for keyboard input
 window.addEventListener("keydown", (e) => {
-  if ((e.key >= 0 && e.key <= 9)) {
-    numbers.forEach((num)=>{
-      if (num.textContent == e.key) num.click()
-    })
-  }
-  else if (e.key === "+" || e.key === "-" || e.key === "/" || e.key === "*"){
-    operators.forEach((op)=>{
-      if (op.textContent == e.key) op.click()
-    })
-  }
-  else if (e.key === "Backspace" || e.key === "Delete") deleteBtn.click()
-  else if (e.key === "=" || e.key === "Enter") equalBtn.click()
-  else if (e.key === ".") decimalBtn.click()
-  console.log(e)
+  if (e.key >= 0 && e.key <= 9) {
+    numbers.forEach((num) => {
+      //matches e.key with num text content
+      if (num.textContent == e.key) num.click();
+    });
+  } else if (e.key === "+" || e.key === "-" || e.key === "/" || e.key === "*") {
+    operators.forEach((op) => {
+      if (op.textContent == e.key) op.click();
+    });
+  } else if (e.key === "Backspace" || e.key === "Delete") deleteBtn.click();
+  else if (e.key === "=" || e.key === "Enter") equalBtn.click();
+  else if (e.key === ".") decimalBtn.click();
 });
-//operator listener and logic
-operators.forEach((op) => {
-  op.addEventListener("click", () => {
-    if (op.textContent == "X") operator = "*"; //handles * text content
-    else operator = op.textContent;
 
-    if (num1 !== "" && num2 !== "") {
-      //if clicked n num1 n 2 not empty then evaluate
-      evaluate(); //ex 1 + 1 + > (2) + next number
-      operator = op.textContent; //maintains operator for next chained operation
-    } else if (num1 !== "" && num2 === "") {
-      num2 = num1; // Move num1 to num2 for chaining on initial input of num1
-      num1 = "";
-    } else if (result !== undefined) {
-      // If equal was pressed, use the result for chaining
-      num2 = result;
-      num1 = "";
-    }
-  });
-});
-decimalBtn.addEventListener("click", () => {
-  if (!num1.includes(".") && num1.length < maxDigits) {
-    if (num1 !== "") {
-      num1 += ".";
-      displayScreen.textContent = num1;
-    } else {
-      num1 += "0.";
-      displayScreen.textContent = num1;
-    }
-    decimalClickedAfterEqual = true;
-  }
-});
-equalBtn.addEventListener("click", () => {
-  evaluate();
-});
 const evaluate = () => {
   //if needed paramaters exist
   if (num1 !== "" && num2 !== "" && operator !== "") {
@@ -129,6 +79,60 @@ const operate = (num1, operator, num2) => {
   else return null;
 };
 
+//listener on numbers buttons
+numbers.forEach((num) => {
+  num.addEventListener("click", () => {
+    if (operator === "" && result !== undefined) {
+      //if evaluation occured and number is clicked next instead of op
+      if (decimalClickedAfterEqual);
+      else num1 = "";
+      num2 = "";
+      result = undefined; // Reset result
+    }
+    if (num1.length < maxDigits) {
+      num1 += num.textContent; // Always append to num1
+      displayScreen.textContent = num1;
+      decimalClickedAfterEqual = false; // Reset the flag after a number is clicked
+    }
+  });
+});
+//operator listener and logic
+operators.forEach((op) => {
+  op.addEventListener("click", () => {
+    if (op.textContent == "X") operator = "*"; //handles * text content
+    else operator = op.textContent;
+
+    if (num1 !== "" && num2 !== "") {
+      //if clicked n num1 n 2 not empty then evaluate
+      evaluate(); //ex 1 + 1 + > (2) + next number
+      operator = op.textContent; //maintains operator for next chained operation
+    } else if (num1 !== "" && num2 === "") {
+      num2 = num1; // Move num1 to num2 for chaining on initial input of num1
+      num1 = "";
+    } else if (result !== undefined) {
+      // If equal was pressed, use the result for chaining
+      num2 = result;
+      num1 = "";
+    }
+  });
+});
+decimalBtn.addEventListener("click", () => {
+  //checks decimal isnt already included
+  if (!num1.includes(".") && num1.length < maxDigits) {
+    if (num1 !== "") {
+      num1 += ".";
+      displayScreen.textContent = num1;
+    } else {
+      num1 += "0.";
+      displayScreen.textContent = num1;
+    }
+    //set decimal tracker to true
+    decimalClickedAfterEqual = true;
+  }
+});
+equalBtn.addEventListener("click", () => {
+  evaluate();
+});
 clearBtn.addEventListener("click", () => {
   num1 = "";
   num2 = "";
